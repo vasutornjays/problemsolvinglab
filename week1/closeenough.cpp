092fde7efcca43fe8c3c7c4eae73151526d1fc5e
vasutornjays <vasutorn.s@ku.th>
2017-01-22 00:56:50 +0700

    add ignore file

--- week1/closeenough.cpp
+++ week1/closeenough.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     int n,l;
-    int house[20]; 
+    int house[20];
+    int count = 0; 
     cin >> n >> l;
     for(int i = 0; i < n ;i++)
     {
-        
+        cin >> house[i];
     }
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = i+1;j < n;j++)
+        {
+            if(abs(house[i] - house[j]) <= l)
+            {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
 }
